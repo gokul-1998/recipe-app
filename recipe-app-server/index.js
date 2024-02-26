ea021ba61554d815d35005a8a707cfac8a2feecc
Santhosh <santhoshs19032003@gmail.com>
2024-02-26 10:56:31 +0530

    post request for recipe

---
+++ recipe-app-server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const recipeSchema = require("./models/recipe.schema");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// MongoDB Connection String
+
+mongoose.connect(
+  `mongodb+srv://21f1007026:${process.env.MONGODB_URI_PASSWORD}@cluster0.89v8kpt.mongodb.net/?retryWrites=true&w=majority`
+);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+// Initial Route
+app.get("/", (req, res) => {
+  return res.status(200).send("Welcome Home");
+});
+
+// Create Recipe
+app.post("/recipe", async (req, res) => {
+  console.log(req.body);
+  try {
+    const {
+      name,
+      thumbnail,
+      images,
+      description,
+      ingredients,
+      instructions,
+      nutritionalValues,
+      difficulty,
+      prepTime,
+    } = req.body;
+    const data = await recipeSchema.create({
+      name,
+      thumbnail,
+      images,
+      description,
+      ingredients,
+      instructions,
+      nutritionalValues,
+      difficulty,
+      prepTime,
+    });
+    data.save();
+    return res.status(200).send("Recipe Saved Successfully");
+  } catch (error) {
+    res.status(501).send(error);
+  }
+});
---
+++ recipe-app-server/models/recipe.schema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  thumbnail: { type: String, required: true },
+  images: [String],
+  description: { type: String, required: true },
+  ingredients: [
+    {
+      name: { type: String, required: true },
+      quantity: { type: String, required: true },
+    },
+  ],
+  instructions: [{ type: String, required: true }],
+  nutritionalValues: {
+    calories: { type: String },
+    protein: { type: String },
+    carbohydrates: { type: String },
+    fat: { type: String },
+  },
+  difficulty: { type: String },
+  prepTime: { type: String },
+  createdDate: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
